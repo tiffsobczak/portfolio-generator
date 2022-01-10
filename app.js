727b22515e54f9430389e25ec65b4cf3d1cfe599
Tiffany <tiff.sobczak@gmail.com>
2022-01-10 14:06:23 -0600

    completed module

--- app.js
+++ app.js
@@ -129,11 +129,28 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
+      return generatePage(portfolioData);
+  })
+  .then(pageHTML=> {
+      return writeFile(pageHTML);
+  })
+  .then(writeFileResponse=> {
+      console.log(writeFileResponse);
+      return fs.copyFile();
+  })
+  .then(copyFileResponse=> {
+      console.log(copyFileResponse);
+  })
+  .catch(err=> {
+      console.log(err);
+  });
     const pageHTML = generatePage(portfolioData);
 
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
+    fs.writeFile('./dist/index.html', pageHTML, err => {
+      if (err) {
+      console.log(err);
+      return;
+      }
       console.log('Page created! Check out index.html in this directory to see it!');
-    });
+
   });
